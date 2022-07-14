01989435d172e41f0695e066e81b23b635ee8a21
Sergey <s.p.web@ya.ru>
2022-07-14 23:42:18 +0300

    task15

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿System.Console.WriteLine("Введите номер дня недели");
+int Num = Convert.ToInt32(Console.ReadLine());
+if (Num > 7)
+{
+System.Console.WriteLine("Нет такого дня недели");
+}
+else 
+{
+bool isMultiplicity (int Num)
+{
+    return(Num == 7 || Num == 6 );
+}
+Console.WriteLine(isMultiplicity(Num));
+}
+
+// так тоже работает 
+// int Num = Convert.ToInt32(Console.ReadLine());
+
+// if (Num > 7)
+// {
+// System.Console.WriteLine("Нет такого дня недели");
+// }
+// else 
+// {
+//     if (Num <=5)
+//     {
+//         System.Console.WriteLine("нет");
+//     }
+//     else 
+//     System.Console.WriteLine("да");
+// }
